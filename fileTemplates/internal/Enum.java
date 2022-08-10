be0e0183d412e80c12ab9dd69264c35987506dd2
lenovo <lenovo@LAPTOP-1K79C1JD>
2022-08-10 22:18:23 +0800

    IC-2021.3.2 <lenovo@LAPTOP-1K79C1JD Create baseRefactoring.xml, _@user_Arc Dark _Material_.icls, ide.general.xml, _@user_One Dark Vivid Italic.icls, ide-features-trainer.xml, _@user_Atom One Dark _Material_.icls, project.default.xml, rainbow_brackets.xml, Enum.java, shared-indexes.xml, Default.xml, _@user_Material Deep Ocean.icls, find.xml, ui.lnf.xml, _@user_Nord.icls, filetypes.xml, checkstyle-idea.xml, pinyin_completion_helper.xml, keymap.xml, colors.scheme.xml, _@user_Material Oceanic.icls, KeyPromoterXSettings.xml, _@user_Darcula.icls, _@user_SynthWave _84 _Material_.icls, _@user_Moonlight _Material_.icls, laf.xml, Windows.xml, _@user_Atom One Light _Material_.icls, _@user_Material Darker.icls, _@user_Material Palenight.icls, _@user_Night Owl _Material_.icls, material_custom_theme.xml, _@user_High contrast.icls, Class.java, jdk.table.xml, _@user_Material Sky Blue.icls, _@user_Material Volcano.icls, _@user_Solarized Dark _Material_.icls, material_theme.xml, vcs.xml, codota.xml, grazie_global.xml, _@user_GitHub Dark _Material_.icls, _@user_Material Forest.icls, editor.xml, _@user_Monokai Pro _Material_.icls, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+*   @authhor wu
+*   @date  ${YEAR}/${MONTH}/${DAY} ${HOUR}:${MINUTE}
+*/
+public enum ${NAME} {
+}
